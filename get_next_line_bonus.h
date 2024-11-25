e6b4073a9494b2af273a61323d68b5de61f10d30
Mustafa Ozer <musozer@k1m35s02.42istanbul.com.tr>
2024-11-25 17:06:47 +0300

    getnextline

---
+++ get_next_line_bonus.h
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: musozer <musozer@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/21 10:07:15 by musozer           #+#    #+#             */
+/*   Updated: 2023/11/21 10:07:17 by musozer          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
+
+# ifndef OPEN_MAX
+#  define OPEN_MAX 10240
+# endif
+
+# include <stdlib.h>
+# include <unistd.h>
+
+size_t	ft_strlen(const char *s);
+char	*ft_strdup(const char *s1);
+char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_substr(char const *s, unsigned int start, size_t len);
+char	*ft_read(int fd, char *str, char *src);
+char	*ft_clean(char	*line);
+char	*get_next_line(int fd);
+char	*ft_strchr(const char *s, int c);
+
+#endif
